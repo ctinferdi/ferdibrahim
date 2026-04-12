b9ddbdcab5f60108cb87c8661d134d2e4a4f7120
ctinferdi <ctinferdi@gmail.com>
2026-04-12 22:27:54 +0300

    fix: Update cron job endpoint to check-notifier

--- database/SUPABASE_SCHEDULE_CRON.sql
+++ database/SUPABASE_SCHEDULE_CRON.sql
@@ -7,7 +7,7 @@ SELECT cron.schedule(
   '0 8 * * *',
   $$
   SELECT net.http_post(
-    url := 'https://bqwqwgcrnrtunwzyajzf.supabase.co/functions/v1/smooth-endpoint',
+    url := 'https://bqwqwgcrnrtunwzyajzf.supabase.co/functions/v1/check-notifier',
     headers := '{"Content-Type": "application/json", "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJxd3F3Z2NybnJ0dW53enlhanpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg1MTI4MTYsImV4cCI6MjA4NDA4ODgxNn0.PDSTe3R_utf0zu7pZkW_xHkqcgIhXilCRjZraTZUbqY"}'::jsonb,
     body := '{"time": "' || now() || '"}'::jsonb
   ) as request_id;
